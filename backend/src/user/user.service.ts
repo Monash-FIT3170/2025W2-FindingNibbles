b8ee76ab12bcbe49f17974913c649d4a99b11bae
Jack <mosesjack@gmail.com>
2025-05-03 18:15:10 +1000

    backend for userFavourited restaurants

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -25,6 +25,52 @@ export class UserService {
   async update(id: number, updateUserDto: Prisma.UserUpdateInput) {
     return this.db.user.update({ where: { id }, data: updateUserDto });
   }
+  /**
+   * Create a new user favourite restaurant
+   * @param userId 
+   * @param restaurantId 
+   * @returns 
+   */
+  async favouriteRestaurant(userId: number, restaurantId: number) {
+    return this.db.userFavouritedRestaurant.create({
+      data: {
+        userId,
+        restaurantId,
+      },
+    })
+  }
+  /**
+   * Unfavourite a restaurant
+   * @param userId 
+   * @param restaurantId 
+   * @returns 
+   */
+
+  async unfavouriteRestaurant(userId: number, restaurantId: number) {
+    return this.db.userFavouritedRestaurant.delete({
+      where: {
+        userId_restaurantId: {
+          userId,
+          restaurantId,
+        },
+      },
+    });
+  }
+  /**
+   * Return all the restaurants favourited by a user
+   * @param userId 
+   * @returns 
+   */
+  async getFavouritedRestaurants(userId: number) {
+    return this.db.userFavouritedRestaurant.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        restaurant: true,
+      },
+    });
+  }
 
   async remove(id: number) {
     return this.db.user.delete({ where: { id } });
