c087ffb8ce347461ea0af22dd4c311d718d3e182
moffdog1 <imof0001@student.monash.edu>
2025-05-03 13:32:43 +1000

    feat: Adding location services

--- frontend/lib/pages/map_page.dart
+++ frontend/lib/pages/map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -10,12 +11,52 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  late final MapController _mapController;
+  late final MapController _mapController = MapController();
+  LatLng? _currentPosition;
 
   @override
   void initState() {
     super.initState();
-    _mapController = MapController();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check location services
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    // Permissions
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied.');
+    }
+
+    // Get current position
+    final position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _currentPosition = LatLng(position.latitude, position.longitude);
+    });
+
+    // Listen for updates
+    Geolocator.getPositionStream().listen((Position pos) {
+      final newPos = LatLng(pos.latitude, pos.longitude);
+      setState(() {
+        _currentPosition = newPos;
+      });
+      _mapController.move(newPos, _mapController.zoom);
+    });
   }
 
   @override
