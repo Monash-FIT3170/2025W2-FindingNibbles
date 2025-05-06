f16704a3779fa5fc68284be21ef83608640d2f16
Lachlan MacPhee <github@lachlanmacphee.com>
2025-05-06 23:33:04 +1000

    fix: protect recipe endpoints

--- backend/src/recipe/recipe.controller.ts
+++ backend/src/recipe/recipe.controller.ts
@@ -5,15 +5,18 @@ import {
   Body,
   Param,
   NotFoundException,
+  UseGuards,
 } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { Prisma } from 'generated/prisma';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt/jwt-auth.guard';
 
 @Controller('recipe')
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createRecipeDto: CreateRecipeDto) {
     const generatedRecipe = await this.recipeService.generate(createRecipeDto);
@@ -39,11 +42,13 @@ export class RecipeController {
     return this.recipeService.create(recipe);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.recipeService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.recipeService.findOne(+id);
