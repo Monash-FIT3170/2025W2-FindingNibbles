4f5594237b647ed4dca9919d26d4790380ed1c78
Lachlan MacPhee <github@lachlanmacphee.com>
2025-05-06 23:43:37 +1000

    fix: guard no longer needed as default protected

--- backend/src/recipe/recipe.controller.ts
+++ backend/src/recipe/recipe.controller.ts
@@ -5,18 +5,15 @@ import {
   Body,
   Param,
   NotFoundException,
-  UseGuards,
 } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { Prisma } from 'generated/prisma';
-import { JwtAuthGuard } from 'src/auth/strategies/jwt/jwt-auth.guard';
 
 @Controller('recipe')
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createRecipeDto: CreateRecipeDto) {
     const generatedRecipe = await this.recipeService.generate(createRecipeDto);
@@ -42,13 +39,11 @@ export class RecipeController {
     return this.recipeService.create(recipe);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.recipeService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.recipeService.findOne(+id);
