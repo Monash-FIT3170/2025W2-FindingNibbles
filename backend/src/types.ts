a786f46ca96753a9d7ce3fbc96d91d8d6e367f56
Lachlan MacPhee <github@lachlanmacphee.com>
2025-05-06 23:43:07 +1000

    feat: protect all routes by default

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,10 +1,15 @@
 import { Request as ExpressRequest } from 'express';
 
-export interface RequestUser extends  ExpressRequest{
-    user: {
-      id: number;
-      email: string;
-      firstName: string;
-      lastName: string;
-    };
-  }
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export interface RequestUser extends ExpressRequest {
+  user: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+  };
+}
