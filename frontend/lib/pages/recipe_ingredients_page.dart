55894eab21ada73f032dc384860ffd08c9683984
saveradisanayaka <savera.d@gmail.com>
2025-05-07 23:21:06 +1000

    worked on the first widget to display the ingredients page

--- frontend/lib/pages/recipe_ingredients_page.dart
+++ frontend/lib/pages/recipe_ingredients_page.dart
@@ -2,4 +2,21 @@ import 'package:flutter/material.dart';
 
 class RecipeIngredientsPage extends StatelessWidget {
   const RecipeIngredientsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Card(
+      shadowColor: Colors.transparent,
+      margin: const EdgeInsets.all(8.0),
+      child: SizedBox.expand(
+        child: Center(
+          child: Text('Recipe Ingredients page', style: theme.textTheme.titleLarge),
+        ),
+      ),
+    );
+  }
 }
+
+// This widget is used to display the recipe ingredients page.  
+// It is a stateless widget that builds a card with a centered text.  
