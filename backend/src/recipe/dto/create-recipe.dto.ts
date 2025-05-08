6dcc7bdaa4ed8a1a2e691cd2251ae49974f3c80a
Oliver <oliver.sirota@gmail.com>
2025-05-08 22:28:55 +1000

    Updated DTO with RecipeDifficulty as an enum

--- backend/src/recipe/dto/create-recipe.dto.ts
+++ backend/src/recipe/dto/create-recipe.dto.ts
@@ -1,29 +1,31 @@
 import {
   IsNotEmpty,
-  ArrayNotEmpty,
   IsBoolean,
-  IsIn,
   IsArray,
+  IsEnum,
 } from 'class-validator';
 
-export const RecipeDifficultyLevels = ['easy', 'medium', 'hard', 'any'];
-export type RecipeDifficultyLevel = (typeof RecipeDifficultyLevels)[number];
+export enum RecipeDifficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+  ANY = 'any',
+}
 
 export class CreateRecipeDto {
-
   @IsArray()
   ingredients: string[];
 
   @IsArray()
-  dietaryRequirements: string[];
+  dietaryRequirements: string[]; // Can include 'saved', custom tags, or both
 
   @IsArray()
-  kitchenAppliances: number[];
+  kitchenAppliances: number[]; // Appliance IDs
 
   @IsBoolean()
   @IsNotEmpty()
   includeAllIngredients: boolean;
 
-  @IsIn(RecipeDifficultyLevels)
-  difficultyLevel: RecipeDifficultyLevel;
+  @IsEnum(RecipeDifficulty)
+  difficulty_level: RecipeDifficulty;
 }
