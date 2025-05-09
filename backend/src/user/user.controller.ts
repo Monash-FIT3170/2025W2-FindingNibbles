8cb5b8df718f1fbe2095db7f80d3002e65b1f8d0
Lachlan MacPhee <github@lachlanmacphee.com>
2025-05-10 08:13:25 +1000

    fix: cleanup user controller routes

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,59 +1,38 @@
 import { Body, Controller, Post, Req, Get, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/strategies/jwt/jwt-auth.guard';
 import { RequestUser } from 'src/types';
-import { CreateDietaryRestrictionDto } from 'src/dietary-restriction/dto/create-dietary-restriction.dto';
 
 @Controller('user')
 export class UserController {
   constructor(readonly userService: UserService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post('favourite-restaurant')
   favouriteRestaurant(@Req() req: RequestUser, @Body() restaurantId: number) {
     return this.userService.favouriteRestaurant(req.user.id, restaurantId);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Post('unfavourite-restaurant')
+  @Delete('favourite-restaurant')
   unfavouriteRestaurant(@Req() req: RequestUser, @Body() restaurantId: number) {
     return this.userService.unfavouriteRestaurant(req.user.id, restaurantId);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('favourite-restaurant')
   getFavouritedRestaurants(@Req() req: RequestUser) {
     return this.userService.getFavouritedRestaurants(req.user.id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post('dietary-restriction')
   addDietaryRestriction(@Req() req: RequestUser, @Body() dietaryId: number) {
     return this.userService.addDietaryRestriction(req.user.id, dietaryId);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('dietary-restriction')
   removeDietaryRestriction(@Req() req: RequestUser, @Body() dietaryId: number) {
     return this.userService.removeDietaryRestriction(req.user.id, dietaryId);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('dietary-restriction')
   getUserDietaryRestrictions(@Req() req: RequestUser) {
     return this.userService.getDietaryRestrictions(req.user.id);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('create-dietary-restriction')
-  createDietaryRestriction(
-    @Req() req: RequestUser,
-    @Body() dietaryRestriction: CreateDietaryRestrictionDto,
-  ) {
-    return this.userService.createUserSpecificDietaryRestriction(
-      req.user.id,
-      dietaryRestriction,
-    );
   }
 }
