8138cdde41478ef62a2bca8e185da44a45037eb7
Jeremia Yovinus <jeremiayo@gmail.com>
2025-05-14 22:51:39 +1000

    made simple initialisaiton to recipe instruction page

--- frontend/lib/pages/recipes/recipe_instructions_page.dart
+++ frontend/lib/pages/recipes/recipe_instructions_page.dart
@@ -9,3 +9,39 @@ class RecipeInstructionsPage extends StatefulWidget {
   @override
   State<RecipeInstructionsPage> createState() => _RecipeInstructionsPageState();
 }
+
+class _RecipeInstructionsPageState extends State<RecipeInstructionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const BackButton(color: Colors.black),
+        centerTitle: false,
+        title: const Text(
+          'Recipe List',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_horiz, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Your image + gradient + title + time indicator + fav icon
+          // Tab selector
+          // Instructions list
+        ],
+      ),
+    );
+  }
+}
