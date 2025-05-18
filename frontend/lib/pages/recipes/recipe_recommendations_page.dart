9639c95b169709a2ee5984898d9f992928c571f9
saveradisanayaka <savera.d@gmail.com>
2025-05-18 19:51:22 +1000

    connected ingredients page so we can navigate on click. Moved difficulty function.

--- frontend/lib/pages/recipes/recipe_recommendations_page.dart
+++ frontend/lib/pages/recipes/recipe_recommendations_page.dart
@@ -1,7 +1,69 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
+import 'recipe_ingredients_page.dart';
 
-class RecipePage extends StatelessWidget {
+class RecipePage extends StatefulWidget {
+  const RecipePage({super.key});
+
+  @override
+  State<RecipePage> createState() => _RecipePageState();
+}
+
+class _RecipePageState extends State<RecipePage> {
+  // Sample data for recipes
+  final List<List<Map<String, String>>> recipePages = [
+    [
+      {
+        'title': 'Traditional spare ribs baked',
+        'time': '55 min',
+        'difficulty': 'Hard',
+        'image': 'assets/images/spare_ribs.jpg',
+      },
+      {
+        'title': 'Spice Roasted Chicken with Flavored Rice',
+        'time': '30 min',
+        'difficulty': 'Medium',
+        'image': 'assets/images/roasted_chicken.jpg',
+      },
+      {
+        'title': 'Spicy fried rice mix chicken bali',
+        'time': '25 min',
+        'difficulty': 'Medium',
+        'image': 'assets/images/fried_rice.jpg',
+      },
+    ],
+    [
+      {
+        'title': 'Grilled Salmon with Lemon Butter',
+        'time': '40 min',
+        'difficulty': 'Easy',
+        'image': 'assets/images/grilled_salmon.jpg',
+      },
+      {
+        'title': 'Vegetarian Pasta Primavera',
+        'time': '35 min',
+        'difficulty': 'Easy',
+        'image': 'assets/images/pasta_primavera.jpg',
+      },
+      {
+        'title': 'Beef Stroganoff',
+        'time': '50 min',
+        'difficulty': 'Hard',
+        'image': 'assets/images/beef_stroganoff.jpg',
+      },
+    ],
+  ];
+
+    // Function to determine the difficulty color
+  Color _getDifficultyColor(String difficulty) {
+    if (difficulty == 'Hard') { // hard will be red 
+      return Colors.red;
+    } else if (difficulty == 'Medium') { // medium will be orange
+      return Colors.orange;
+    } else {
+      return Colors.green; // easy will be green
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,14 +116,4 @@ class RecipePage extends StatelessWidget {
     );
   }
 
-  // Function to determine the difficulty color
-  Color _getDifficultyColor(String difficulty) {
-    if (difficulty == 'Hard') {
-      return Colors.red;
-    } else if (difficulty == 'Medium') {
-      return Colors.orange;
-    } else {
-      return Colors.green;
-    }
   }
-}
