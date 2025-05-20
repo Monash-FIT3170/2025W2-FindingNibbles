53fbe22fdbdc664e2c65745b4a3f0834f0cf0c75
Kehan Ranasinghe <kehanran51423@gmail.com>
2025-05-20 17:09:53 +1000

    fix: revert to original validate() in jwt.strategy.ts

--- backend/src/auth/strategies/jwt/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt/jwt.strategy.ts
@@ -22,11 +22,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   // We can assume that the payload is already validated.
   validate(payload: JwtPayload) {
-    return {
-      id: payload.sub, // Extract user ID from sub claim
-      email: payload.email,
-      firstName: payload.firstName,
-      lastName: payload.lastName,
-    };
+    return payload;
   }
 }
