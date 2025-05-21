70db9ce802fcf3af563674dc441113bbf004bdfc
Lachlan MacPhee <github@lachlanmacphee.com>
2025-05-22 01:31:38 +1000

    fix: sleepy with the env var

--- backend/src/recipe/recipe.service.ts
+++ backend/src/recipe/recipe.service.ts
@@ -122,7 +122,7 @@ export class RecipeService {
         }>;
       };
       try {
-        const model = this.configService.get<string>('GOOGLE_GEMINI_MODEL');
+        const model = this.configService.get<string>('GOOGLE_GEMINI_API_MODEL');
         const apiKey = this.configService.get<string>('GOOGLE_GEMINI_API_KEY');
         const response = await fetch(
           `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`,
