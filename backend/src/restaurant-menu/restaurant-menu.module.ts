8e8f6114d334f06f2c1b32423e81bead28318d12
Lachlan MacPhee <github@lachlanmacphee.com>
2025-08-12 22:54:21 +1000

    fix: address jerey's pr feedback

--- backend/src/restaurant-menu/restaurant-menu.module.ts
+++ backend/src/restaurant-menu/restaurant-menu.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { RestaurantMenuService } from './restaurant-menu.service';
 import { RestaurantMenuController } from './restaurant-menu.controller';
 import { DatabaseModule } from '../database/database.module';
+import { GeminiModule } from '../gemini/gemini.module';
 
 @Module({
   controllers: [RestaurantMenuController],
   providers: [RestaurantMenuService],
-  imports: [DatabaseModule, ConfigModule],
+  imports: [DatabaseModule, ConfigModule, GeminiModule],
 })
 export class RestaurantMenuModule {}
