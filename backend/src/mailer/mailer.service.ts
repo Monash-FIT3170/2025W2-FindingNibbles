d880b0b0b22788aed0895d2baeef9408bf155840
Lachlan MacPhee <github@lachlanmacphee.com>
2025-09-12 12:07:28 +1000

    fix: registration email - change env

--- backend/src/mailer/mailer.service.ts
+++ backend/src/mailer/mailer.service.ts
@@ -4,41 +4,33 @@ import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class MailerService {
-  private fromEmail: string;
   private transporter: nodemailer.Transporter;
 
   constructor(private readonly configService: ConfigService) {
-    this.fromEmail =
-      this.configService.get<string>('MAIL_USER') || 'findingnibbles@gmail.com';
-
-    const port = this.configService.get<number>('MAIL_PORT') || 587;
     this.transporter = nodemailer.createTransport({
-      host: this.configService.get<string>('MAIL_HOST'),
-      port,
-      secure: port === 465,
+      service: 'gmail',
       auth: {
-        user: this.configService.get<string>('MAIL_USER'),
-        pass: this.configService.get<string>('MAIL_PASS'),
+        user: this.configService.get<string>('GOOGLE_MAIL_USER'),
+        pass: this.configService.get<string>('GOOGLE_MAIL_PASS'),
       },
     });
   }
 
   sendMail(to: string, subject: string, html: string) {
-    const fromEmail = this.configService.get<string>('MAIL_USER');
-    const mailOptions = {
-      from: `"FindingNibbles" <${fromEmail}>`,
-      to,
-      subject,
-      html,
-    };
-
-    this.transporter.sendMail(mailOptions, (error) => {
-      if (error) {
-        console.error('Error sending email:', error);
-      } else {
-        console.log('Email sent successfully');
-      }
-    });
+    this.transporter.sendMail(
+      {
+        to,
+        subject,
+        html,
+      },
+      (error) => {
+        if (error) {
+          console.error('Error sending email:', error);
+        } else {
+          console.log('Email sent successfully');
+        }
+      },
+    );
   }
 
   sendVerificationEmail(email: string, code: number) {
